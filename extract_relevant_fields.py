cd4f07b40c4c90709f0485fb30ea8cc45bdebbda
RonFed <ronfederman@mail.tau.ac.il>
2023-05-20 16:16:30 +0300

    Add experience and skills

--- extract_relevant_fields.py
+++ extract_relevant_fields.py
@@ -11,6 +11,8 @@ def extract_relevant_fields(soup):
 
     # Education
     education = []
+    skills = []
+    experience = []
     sections = soup.find_all('section')
     for section in sections:
         if section.find('div', {'id': 'education'}) is not None:
@@ -20,8 +22,30 @@ def extract_relevant_fields(soup):
                 for info in item.find_all('span', {'aria-hidden': 'true'}):
                     info_list.append(info.get_text().strip())
                 education.append(info_list)
+        if section.find('div', {'id': 'skills'}) is not None:
+            ul = section.find('ul')
+            for item in ul.find_all('li'):
+                info_list = []
+                for info in item.find_all('span', {'aria-hidden': 'true'}):
+                    info_text = info.get_text().strip()
+                    if "endorse" not in info_text.lower() and len(info_text) > 2:
+                        info_list.append(info.get_text().strip())
+                if len(info_list) > 0:
+                    skills.append(info_list)
+        if section.find('div', {'id': 'experience'}) is not None:
+            ul = section.find('ul')
+            for item in ul.find_all('li'):
+                info_list = []
+                for info in item.find_all('span', {'aria-hidden': 'true'}):
+                    info_list.append(info.get_text().strip())
+                if len(info_list) > 0:
+                    experience.append(info_list)
 
     
-    row = {'Name': name, 'Workplace': works_at, 'Education': education}
+    row = {'Name': name,
+           'Workplace': works_at,
+           'Education': education,
+           'skills': skills,
+           'Experience': experience}
 
     return row
