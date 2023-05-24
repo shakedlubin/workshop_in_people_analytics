2f99920a6ba658bd1f7ae4a975893c39e467adc9
Shaked Lubin <shaked.lubin@gmail.com>
2023-05-24 18:59:43 +0300

    Some cleanup

--- model.py
+++ model.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
-import collections
 from ast import literal_eval
 import re
 from nltk.corpus import wordnet
-from nltk import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+
+INPUT_MSC_FILE = 'msc_data.csv'
+INPUT_NOT_MSC_FILE = 'msc_data.csv'
 
 relevant_words = ['teacher', 'teaching', 'teach']
 similar_words = set()
@@ -18,17 +23,8 @@ for word in relevant_words:
             similar_words.add(lemma.name())
 similar_words = list(similar_words)
 
-def remove_special_characters(text):
-    pattern = r'[^\x00-\x7F]'  # Keep ASCII characters
-    cleaned_text = re.sub(pattern, '', text)
-    return cleaned_text
-
-def check_teaching_exprience(arr):
-    for item in arr:
-        for word in similar_words:
-            if item[0].find(word) != -1:
-                return True
-    return False
+def remove_punctuation(text):
+    return re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
 
 def clean_column(series: pd.Series, filter_pattern: str):
     for row_index in range(len(series)):
@@ -45,64 +41,114 @@ def clean_column(series: pd.Series, filter_pattern: str):
         except:
             continue
 
-def remove_punctuation(text):
-    return re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+def add_cleaned_columns(df):
+    df['Education_clean'] = df['Education']
+    df['Experience_clean'] = df['Experience']
+    df['skills_clean'] = df['skills']
 
-# Data manipulation
-df = pd.read_csv('msc_data.csv', encoding='utf-8')
+    clean_column(df['Education_clean'], r"\b\d{4}\b(?:\s*-\s*(?:\d{4}|[A-Za-z]{3} \d{4}))?")
+    clean_column(df['Experience_clean'], r"(\d+ (?:yr|yrs|mos))")
+    clean_column(df['skills_clean'], r"\b\d{4}\b(?:\s*-\s*(?:\d{4}|[A-Za-z]{3} \d{4}))?")
 
-# Add target column
-np.random.seed(42)
-df['is_msc'] = np.random.choice([True, False], size=len(df))
+    df['Education_clean'] = df['Education_clean'].apply(lambda x: remove_punctuation(x))
+    df['Experience_clean'] = df['Experience_clean'].apply(lambda x: remove_punctuation(x))
+    df['skills_clean'] = df['skills_clean'].apply(lambda x: remove_punctuation(x))
+    
+    return df
 
-df['Education_clean'] = df['Education']
-df['Experience_clean'] = df['Experience']
-df['skills_clean'] = df['skills']
+def remove_special_characters(text):
+    pattern = r'[^\x00-\x7F]'  # Keep ASCII characters
+    cleaned_text = re.sub(pattern, '', text)
+    return cleaned_text
 
-clean_column(df['Education_clean'], r"\b\d{4}\b(?:\s*-\s*(?:\d{4}|[A-Za-z]{3} \d{4}))?")
-clean_column(df['Experience_clean'], r"(\d+ (?:yr|yrs|mos))")
-clean_column(df['skills_clean'], r"\b\d{4}\b(?:\s*-\s*(?:\d{4}|[A-Za-z]{3} \d{4}))?")
+def check_teaching_exprience(arr):
+    for item in arr:
+        for word in similar_words:
+            if item[0].find(word) != -1:
+                return True
+    return False
 
-df['Education'] = df['Education'].apply(lambda x: remove_special_characters(x))
-df['Experience'] = df['Experience'].apply(lambda x: remove_special_characters(x))
-df['skills'] = df['skills'].apply(lambda x: remove_special_characters(x))
+def add_features(df):
+    df['Education'] = df['Education'].apply(lambda x: remove_special_characters(x))
+    df['Experience'] = df['Experience'].apply(lambda x: remove_special_characters(x))
+    df['skills'] = df['skills'].apply(lambda x: remove_special_characters(x))
 
-df['Education'] = df['Education'].apply(lambda x: literal_eval(x))
-df['Experience'] = df['Experience'].apply(lambda x: literal_eval(x))
-df['skills'] = df['skills'].apply(lambda x: literal_eval(x))
+    df['Education'] = df['Education'].apply(lambda x: literal_eval(x))
+    df['Experience'] = df['Experience'].apply(lambda x: literal_eval(x))
+    df['skills'] = df['skills'].apply(lambda x: literal_eval(x))
 
-df['num_of_educations'] = df['Education'].apply(lambda x: len(x))
-df['num_of_experience'] = df['Experience'].apply(lambda x: len(x))
-df['num_of_skills'] = df['skills'].apply(lambda x: len(x))
+    df['num_of_educations'] = df['Education'].apply(lambda x: len(x))
+    df['num_of_experience'] = df['Experience'].apply(lambda x: len(x))
+    df['num_of_skills'] = df['skills'].apply(lambda x: len(x))
 
-df['teaching_exprience'] = df['Experience'].apply(lambda x: check_teaching_exprience(x))
+    df['teaching_exprience'] = df['Experience'].apply(lambda x: check_teaching_exprience(x))
+
+    return df
+
+def add_nlp_features(df, col_name):
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(df[col_name])
+    df_bow_sklearn = pd.DataFrame(X.toarray(),columns=vectorizer.get_feature_names_out())
+    df_features = pd.concat([df.reset_index(drop=True), df_bow_sklearn.reset_index(drop=True)], axis=1)
+
+    return df_features
+
+def remove_unwanted_df(df):
+    columns_to_del = ['Education', 'Education_clean', 'Experience', 'Experience_clean', 
+                      'skills', 'skills_clean', 'Name', 'Workplace']
+    for col in columns_to_del:
+        del df[col]
+
+    return df
+
+def run_model(df):
+    # Split the data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('is_msc', axis=1), df['is_msc'], test_size=0.2, random_state=42)
+
+    # Create and train your machine learning models
+    models = [
+        ('Logistic Regression', LogisticRegression(C=100.0, random_state=1, solver='lbfgs', multi_class='ovr', max_iter=5000)),
+        ('Decision Tree', DecisionTreeClassifier()),
+        ('Random Forest', RandomForestClassifier()),
+        ('SVM', SVC())
+    ]
+
+    for model_name, model in models:
+        # Train the model
+        model.fit(X_train, y_train)
+
+        # Evaluate the model
+        accuracy = model.score(X_test, y_test)
+        print(f"{model_name} Accuracy: {accuracy:.2f}")
+
+def main():
+    # Read input files
+    msc_df = pd.read_csv(INPUT_MSC_FILE, encoding='utf-8')
+    not_msc_df = pd.read_csv(INPUT_NOT_MSC_FILE, encoding='utf-8')
+
+    # Add labeling column
+    msc_df['is_msc'] = True
+    not_msc_df['is_msc'] = False
+
+    # Union dataframes
+    combined_df = pd.concat([msc_df, not_msc_df], axis=0)
+
+    # Add cleaned columns
+    full_df = add_cleaned_columns(combined_df)
+
+    # Add features
+    full_df = add_features(full_df)
+
+    # Add Bag-Of-Words features
+    full_df = add_nlp_features(full_df, 'Education_clean')
+
+    # Remove unwanted columns
+    full_df = remove_unwanted_df(full_df)
+    
+    # Run model
+    full_df.to_csv("input_for_model.csv", index=False)
+    run_model(full_df)
 
 
-
-# NLP (Bag of words)
-df['Education_clean'] = df['Education_clean'].apply(lambda x: remove_punctuation(x))
-df['tokens'] = df['Education_clean'].apply(lambda x: word_tokenize(x))
-df = df[df['tokens'].apply(lambda x: len(x) > 0)]
-
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(df['tokens'].apply(lambda x: ' '.join(x)))
-df_bow_sklearn = pd.DataFrame(X.toarray(),columns=vectorizer.get_feature_names_out())
-
-# Concatenate the Bag of Words features with the original feature matrix
-selected_columns = ['num_of_educations', 'num_of_experience', 'num_of_skills', 'teaching_exprience']
-df_selected = df[selected_columns]
-df_features = pd.concat([df_selected.reset_index(drop=True), df_bow_sklearn.reset_index(drop=True)], axis=1)
-
-# Split the data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(df_features, df['is_msc'], test_size=0.2, random_state=42)
-
-# Create and train your machine learning model
-model = LogisticRegression(C=100.0, random_state=1, solver='lbfgs', multi_class='ovr')
-model.fit(X_train, y_train)
-
-# Evaluate the model
-accuracy = model.score(X_test, y_test)
-print("Accuracy:", accuracy)
-
-#print(df)
-#df.to_csv('output.csv', index=False)
+if __name__ == '__main__':
+    main()
